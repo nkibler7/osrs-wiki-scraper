ac42c6c72f4ea6e9e1dd233e7b0b55956891df4c
nkibler7@gmail.com <c6@VhUvrAOoAFwB5>
2021-01-03 15:40:39 -0500

    Adds the first version of the scraper, complete with Docker for containerized execution.

---
+++ main.py
@@ -0,0 +1,65 @@
+import mwparserfromhell
+import os
+import pywikibot
+import sys
+import util
+from google.protobuf.json_format import ParseDict, ParseError
+from proto.gen.npc_infos_pb2 import NpcInfos, NpcInfo
+from pywikibot import family, pagegenerators
+from warnings import warn
+
+
+class OSRSFamily(family.Family):
+    """A pywikibot Family class for the OSRS Wiki."""
+    name = 'osrs'
+    langs = {
+        'en': 'oldschool.runescape.wiki',
+    }
+
+    def protocol(self, code):
+        return 'HTTPS'
+
+    def scriptpath(self, code):
+        return ''
+
+
+def parse_and_write_npcinfos(output_dir: str):
+    npc_infos = NpcInfos()
+
+    site = pywikibot.Site(fam=OSRSFamily(), code='en')
+    category = pywikibot.Category(site, 'Category:Monsters')
+    gen = pagegenerators.PreloadingGenerator(category.articles())
+    for page in gen:
+        code = mwparserfromhell.parse(page.get(), skip_style_tags=True)
+
+        for infobox in util.get_infobox_versions(code):
+            # Skips this infobox if the "removal" key is set.
+            if 'removal' in infobox and not str(infobox['removal']).strip().lower() in ['', 'no']:
+                continue
+
+            # Parses string ID fields into an array of ints.
+            ids = [int(npc_id) for npc_id in str(infobox['id']).split(',') if npc_id != '' and npc_id.isdigit()]
+
+            # Deletes all unknown fields and fields with invalid values.
+            known_fields = list(map(lambda f: f.name, NpcInfo.DESCRIPTOR.fields))
+            filtered_infobox = {k: infobox[k] for k in known_fields if k in infobox and util.is_valid(infobox[k])}
+
+            try:
+                npc_info = ParseDict(filtered_infobox, NpcInfo())
+                npc_info.ids[:] = ids
+                npc_infos.npcs.append(npc_info)
+            except ParseError:
+                warn('Failed to parse JSON into NpcInfo proto: {}'.format(filtered_infobox))
+
+    output_filename = os.path.join(output_dir, 'npc_infos.binarypb')
+    util.write_proto(npc_infos, output_filename)
+    print('{} NpcInfos written to: {}'.format(len(npc_infos.npcs), output_filename))
+
+
+def main():
+    output_dir = 'out' if len(sys.argv) == 1 else sys.argv[1]
+    parse_and_write_npcinfos(output_dir)
+
+
+if __name__ == "__main__":
+    main()
---
+++ util.py
@@ -0,0 +1,50 @@
+import errno
+import os
+import re
+from google.protobuf.message import Message
+from typing import *
+
+comment_stripper = re.compile(r'<!--(.*?)-->')
+version_extractor = re.compile(r'(.*?)([0-9]+)?$')
+
+
+def write_proto(message: Message, filename: str):
+    if not os.path.exists(os.path.dirname(filename)):
+        try:
+            os.makedirs(os.path.dirname(filename))
+        except OSError as exc:  # Guard against race condition
+            if exc.errno != errno.EEXIST:
+                raise
+
+    with open(filename, "wb") as f:
+        f.write(message.SerializeToString())
+
+
+def get_infobox_versions(code) -> Iterator[Dict[str, Any]]:
+    infoboxes = code.filter_templates(matches=lambda t: t.name.matches('Infobox Monster'))
+    if len(infoboxes) < 1:
+        return
+    for infobox in infoboxes:
+        base: Dict[str, str] = {}
+        versions: Dict[int, Dict[str, str]] = {}
+        for param in infobox.params:
+            matcher = version_extractor.match(str(param.name).strip())
+            if matcher is None:
+                raise AssertionError()
+            primary = matcher.group(1)
+            dic = base
+            if matcher.group(2) is not None:
+                version = int(matcher.group(2))
+                if version not in versions:
+                    versions[version] = {}
+                dic = versions[version]
+            dic[primary] = comment_stripper.sub('', param.value.strip())
+        if len(versions) == 0:
+            yield base
+        else:
+            for version_id, version_dict in versions.items():
+                yield {**base, **version_dict}
+
+
+def is_valid(value: Any) -> bool:
+    return not isinstance(value, str) or (value != '' and value != 'N/A')
